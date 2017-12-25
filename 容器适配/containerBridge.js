32bee4bf8046a1507f4ee3059f15d884d049330c
114233763@qq.com <mailzy@vip.qq.com>
2017-12-25 18:28:02 +0800

    fix data envelop

--- 容器适配/containerBridge.js
+++ 容器适配/containerBridge.js
@@ -903,7 +903,7 @@ RelBridge.prototype._remoteGetConfig = function () {
             url: url,
             method: "GET",
             onSuccess: function (data) {
-                var remouteConfig = data.result;
+                var remouteConfig = data;
                 var relConfig = {
                     debug: ContainerBridgeConfig.debug ? 1 : 0, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                     appid: remouteConfig.AppId,                 // 必填，公众号的唯一标识
